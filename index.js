6c2f70a70b4cbae17fb260a99f00bde693412d1f
rupokraihan <rupok2321@gmail.com>
2023-06-09 22:56:21 +0600

    role for instructor imlemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.port || 5000;
 
@@ -31,11 +32,10 @@ async function run() {
 
     // users related apis
 
-    app.get('/users', async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
-
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -48,19 +48,33 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-
-    app.patch('/users/admin/:id', async (req, res) => {
+    // this is for admin role
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: 'admin',
-          
-        }
-      }
+          role: "admin",
+        },
+      };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
+
+    // this is for Instructor role
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
 
 
 
